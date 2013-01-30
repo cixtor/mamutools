469beea8648789783ac3bdbcdcf0129ff9198363
Cixtor <cixtords@gmail.com>
2013-01-30 09:00:00 -0500

    Added. LaravelApp Generator: Composer auto-installation

--- laravelapp.sh
+++ laravelapp.sh
@@ -22,6 +22,7 @@
 # to build any application with which you are tasked.
 #
 PROJECT=$1
+COMPOSER=''
 IFS=$'\n'
 SEPARATOR=$(for i in {1..50}; do echo -n '-'; done; echo)
 WRITE_DIRECTORIES=(
@@ -30,6 +31,19 @@ WRITE_DIRECTORIES=(
 	'./app/storage/sessions/'
 	'./app/storage/views/'
 )
+function fail {
+	case "$1" in
+		no_curl )
+			echo "    You need to have CURL or WGet installed in your system to proceed.";;
+		no_php )
+			echo "    You need to have PHP installed in your system to proceed.";;
+		no_composer )
+			echo "    Without composer you can not create a Laravel Application";;
+	esac
+	echo -e "\e[0;91m    Finished.\e[0m"
+	exit
+}
+#
 # Generate application name and clean the variable.
 clear
 echo -e "Laravel Application Generator"
@@ -43,14 +57,51 @@ if [ "${PROJECT}" == "" ]; then
 		PROJECT=$PROJECT_NAME
 	fi
 fi
-# Create project based on the latest version of the Laravel framework.
 PROJECT=$(echo "${PROJECT}" | sed 's/ /-/g')
 echo -e "\e[0;92m[+]\e[0m Creating application structure for: \e[0;93m${PROJECT}\e[0m"
+#
+# Check the existence of Composer in the system.
+if [ $(which composer) ]; then
+	COMPOSER=$(which composer)
+elif [ $(which composer.phar) ]; then
+	COMPOSER=$(which composer.phar)
+else
+	echo -en "\e[0;91m[x] Error:\e[0m Composer was not found, do you want to install it? (Y/n) "
+	read INSTALL_COMPOSER
+	if [ "${INSTALL_COMPOSER}" == "y" ] || [ "${INSTALL_COMPOSER}" == "Y" ]; then
+		CURRENT_PATH=$(pwd)
+		if [ $(which curl) ]; then
+			if [ $(which php) ]; then
+				echo -n "    Installing Composer... "
+				$(which curl) -sS 'https://getcomposer.org/installer' | $(which php) > /dev/null
+				COMPOSER="${CURRENT_PATH}/composer.phar"
+				chmod 755 $COMPOSER
+				echo -e "\e[0;92mOK\e[0m"
+			else
+				fail "no_php"
+			fi
+		elif [ $(which wget) ]; then
+			echo -n "    Downloading latest Composer version... "
+			wget --quiet 'http://getcomposer.org/composer.phar' -O 'composer.phar'
+			COMPOSER="${CURRENT_PATH}/composer.phar"
+			chmod 755 $COMPOSER
+			echo -e "\e[0;92mOK\e[0m"
+		else
+			fail "no_curl"
+		fi
+	else
+		fail "no_composer"
+	fi
+fi
+echo -en "\e[0;92m[+]\e[0m Composer found at: "
+echo $COMPOSER
 echo $SEPARATOR
-composer.phar -vv create-project laravel/laravel "${PROJECT}" --prefer-dist
-# Clean up project (remove useless directories).
+#
+# Create project based on the latest version of the Laravel framework.
+mkdir -p "${PROJECT}"
 if [ -e "${PROJECT}" ]; then
 	cd "${PROJECT}"
+	$COMPOSER -vv create-project laravel/laravel ./ --prefer-dist
 	CURRENT_SIZE=$(du -sh ./ | awk '{print $1}')
 	DIRECTORIES=()
 	for DIRECTORY in $(find ./ -type d -iname ".git"); do
