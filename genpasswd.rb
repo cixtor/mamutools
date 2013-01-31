b12d69a178dbb0e4c7f3cf70f9f41cc65c2da8d9
Cixtor <cixtords@gmail.com>
2013-01-31 09:00:00 -0500

    Added. Password Generator: Script information

--- genpasswd.rb
+++ genpasswd.rb
@@ -1,4 +1,29 @@
 #!/usr/bin/env ruby
+#
+# Password Generator
+# http://www.cixtor.com/
+# http://www.youtube.com/watch?v=BIKV3fYmzRQ
+#
+# A random password generator is software program or hardware device that takes
+# input from a random or pseudo-random number generator and automatically generates
+# a password. Random passwords can be generated manually, using simple sources
+# of randomness such as dice or coins, or they can be generated using a computer.
+#
+# While there are many examples of "random" password generator programs available
+# on the Internet, generating randomness can be tricky and many programs do not
+# generate random characters in a way that ensures strong security. A common
+# recommendation is to use open source security tools where possible, since they
+# allow independent checks on the quality of the methods used. Note that simply
+# generating a password at random does not ensure the password is a strong password,
+# because it is possible, although highly unlikely, to generate an easily guessed
+# or cracked password. In fact there is no need at all for a password to have been
+# produced by a perfectly random process: it just needs to be sufficiently difficult
+# to guess.
+#
+# A password generator can be part of a password manager. When a password policy
+# enforces complex rules, it can be easier to use a password generator based on
+# that set of rules than to manually create passwords.
+#
 require 'rubygems'
 require 'getoptlong'
 #
