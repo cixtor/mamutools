de720316fb071bad03cbecec586035120f427df0
Cixtor <cixtords@gmail.com>
2013-02-01 09:00:00 -0500

    Added. Debian Brightness Control

---
+++ brightness.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Debian Brightness Control
+# @reference:
+#   http://www.cixtor.com/
+#   http://www.cixtor.com/blog/41/brightness-control
+# @usage: This script solves these issues:
+#   http://forums.linuxmint.com/viewtopic.php?f=18&t=138760
+#   http://forums.linuxmint.com/viewtopic.php?f=59&t=135019
+#   http://forums.linuxmint.com/viewtopic.php?f=90&t=129457
+#   http://forums.linuxmint.com/viewtopic.php?f=199&t=135699
+#
+# Brightness is an attribute of visual perception in which a source appears to
+# be radiating or reflecting light. In other words, brightness is the perception
+# elicited by the luminance of a visual target. This is a subjective attribute
+# or property of an object being observed.
+#
+# Screen brightness can often be tricky to control. On many machines, physical
+# hardware switches are missing and software solutions may or may not work well.
+# Make sure to find a working method for your hardware! Too bright screens can
+# cause eye strain. There are many ways to adjust the screen backlight of a
+# monitor, laptop or integrated panel (such as the iMac) using software, but
+# depending on hardware and model, sometimes only some options are available.
+#
