0f6ab09ea85ac69843a446c5ffb1ac658ec7e769
Cixtor <cixtords@gmail.com>
2013-02-03 09:00:00 -0500

    Added. Orthographic Accent

---
+++ accents.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Orthographic Accent
+# http://www.cixtor.com/
+# http://es.wikipedia.org/wiki/Acento_ortogr%C3%A1fico
+#
+# The orthographic accent or 'tilde' is a sign used in Spanish generally over a
+# vowel (a, e, i, o, u). In some Romances languages, as Spanish and Catalan, is
+# often used the diacritic accent to difference some words from others with the
+# same writing but with different meanings and use cases.
+#
+# In several languages ​​the orthographic accent has some variants, including: the
+# acute accent (´), the circumflex accent (^) or grave accent (`). In many languages​​,
+# each type of accent can fall on different types of speech. In Spanish, the acute
+# accent is the only thing that can go over a vowel. In French the circumflex
+# accent is used to indicate the elongation of a vowel by the loss of the letter
+# "s" implosive, although in Portuguese the same accent is intended to indicate
+# the degree of opening of the vowel.
+#
