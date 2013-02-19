c2e5943834001ba65d7b0b83d27101a24af95c42
Cixtor <cixtords@gmail.com>
2013-02-19 09:00:00 -0500

    Added. Google Chrome Upgrade implementation

--- chrome-upgrade.sh
+++ chrome-upgrade.sh
@@ -21,3 +21,86 @@
 # Upgrades can also worsen a product subjectively. A user may prefer an older version
 # even if a newer version functions perfectly as designed.
 #
+VERSION=$2
+ARCHITECTURE=$1
+GOOGLE_FOLDER_PATH='/opt/google/'
+CHROME_FOLDER_PATH="${GOOGLE_FOLDER_PATH}chrome/"
+TEMP_PACKAGE='google-chrome-latest.deb'
+#
+function help {
+	echo 'Google Chrome Upgrade'
+	echo '  http://www.cixtor.com/'
+	echo '  https://github.com/cixtor/mamutools'
+	echo '  http://www.cixtor.com/blog/53/chrome-upgrade'
+}
+function fail {
+	echo -e "\e[0;91m[x] Error.\e[0m ${1}"
+	exit
+}
+function success {
+	echo -e "\e[0;92mOK. \e[0m ${1}"
+}
+function warning {
+	echo -e "\e[0;93m[!]\e[0m ${1}"
+}
+function question {
+	echo -en "\e[0;94m[?]\e[0m ${1}"
+}
+function request_sudo {
+	warning 'Some operations will require Root provileges, type your password to continue:'
+	echo -n '    '
+	if [ $(sudo whoami) == 'root' ]; then
+		success 'Root privileges granted'
+	else
+		fail 'You can not proceed without root privileges.'
+	fi
+}
+function remote_old_version {
+	if [ -e "${CHROME_FOLDER_PATH}" ]; then
+		question 'Remove old versions of Google Chrome from your system (Y/n) '
+		read REMOVE
+		if [ "${REMOVE}" == 'y' ] || [ "${REMOVE}" == 'Y' ]; then
+			sudo rm -rf "${CHROME_FOLDER_PATH}";
+		else
+			fail 'You need to remove all the old versions of Google Chrome to continue.'
+		fi
+	fi
+}
+function goto_google_folder {
+	if [ ! -e "${GOOGLE_FOLDER_PATH}" ]; then sudo mkdir "${GOOGLE_FOLDER_PATH}"; fi
+	if [ -e "${GOOGLE_FOLDER_PATH}" ]; then
+		CWD=$(pwd)
+		cd "${GOOGLE_FOLDER_PATH}"
+		success "Current working directory: ${CWD}"
+	else
+		fail 'Was impossible to continue, Google folder directory was not created.'
+	fi
+}
+function setup_download_package {
+	if [ "${ARCHITECTURE}" == 'i386' ] || [ "${ARCHITECTURE}" == 'amd64' ]; then ARCHITECTURE="${ARCHITECTURE}"; else ARCHITECTURE='i386'; fi
+	if [ "${VERSION}" == 'stable' ] || [ "${VERSION}" == 'beta' ]; then VERSION="${VERSION}"; else VERSION='beta'; fi
+	LATEST_CHROME="https://dl.google.com/linux/direct/google-chrome-${VERSION}_current_${ARCHITECTURE}.deb";
+	echo -e "  Downloading configured package \e[0;93m${VERSION}-${ARCHITECTURE}\e[0m ...";
+	sudo rm -f "${TEMP_PACKAGE}";
+	wget --quiet --continue "${LATEST_CHROME}" -O "${TEMP_PACKAGE}";
+}
+function install_package {
+	setup_download_package
+	if [ -e "${TEMP_PACKAGE}" ]; then
+		echo '  Installing Google Chrome BETA...';
+		dpkg --extract "${TEMP_PACKAGE}" "${CHROME_FOLDER_PATH}";
+		if [ -e "${CHROME_FOLDER_PATH}" ]; then
+			success "Package installed correctly in: \e[0;92m${CHROME_FOLDER_PATH}\e[0m"
+		else
+			fail 'Package installation failed, try again.'
+		fi
+	else
+		fail 'Package download failed, try again.'
+	fi
+}
+#
+request_sudo
+remote_old_version
+goto_google_folder
+install_package
+#
