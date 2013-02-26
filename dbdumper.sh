b12bab389b05a274c4f4c58fb543cf5b0556d7fc
Cixtor <cixtords@gmail.com>
2013-02-26 09:00:00 -0500

    Added. Database Dumper for DB Backups

---
+++ dbdumper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Database Dumper
+# http://www.cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/Database_dump
+#
+# In information technology, a backup, or the process of backing up, refers to
+# the copying and archiving of computer data so it may be used to restore the
+# original after a data loss event.
+#
+# Backups have two distinct purposes. The primary purpose is to recover data
+# after its loss, be it by data deletion or corruption. The secondary purpose
+# of backups is to recover data from an earlier time, according to a user-defined
+# data retention policy, typically configured within a backup application for
+# how long copies of data are required. Though backups popularly represent a
+# simple form of disaster recovery, and should be part of a disaster recovery
+# plan, by themselves, backups should not alone be considered disaster recovery.
+#
+# One reason for this is that not all backup systems or backup applications are
+# able to reconstitute a computer system or other complex configurations such
+# as a computer cluster, active directory servers, or a database server, by
+# restoring only data from a backup.
+#
+# Since a backup system contains at least one copy of all data worth saving,
+# the data storage requirements can be significant. Organizing this storage
+# space and managing the backup process can be a complicated undertaking. A
+# data repository model can be used to provide structure to the storage.
+# Nowadays, there are many different types of data storage devices that are
+# useful for making backups. There are also many different ways in which these
+# devices can be arranged to provide geographic redundancy, data security, and
+# portability.
+#
