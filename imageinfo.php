c754fa3ef30256db7de5875b3768d8e803fdae4e
Cixtor <cixtords@gmail.com>
2013-03-10 09:00:00 -0400

    Added. Image Information

---
+++ imageinfo.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Image Information
+ * http://www.cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://php.net/manual/en/function.getimagesize.php
+ *
+ * The getimagesize() function will determine the size of any given image file
+ * and return the dimensions along with the file type and a height/width text
+ * string to be used inside a normal HTML IMG tag and the correspondant HTTP
+ * content type.
+ *
+ * Exchangeable image file format (Exif, often incorrectly EXIF) is a standard
+ * that specifies the formats for images, sound, and ancillary tags used by
+ * digital cameras (including smartphones), scanners and other systems handling
+ * image and sound files recorded by digital cameras. The specification uses the
+ * following existing file formats with the addition of specific metadata tags:
+ * JPEG Discrete cosine transform (DCT) for compressed image files, TIFF Rev.
+ * 6.0 (RGB or YCbCr) for uncompressed image files, and RIFF WAV for audio files
+ * (Linear PCM or ITU-T G.711 μ-Law PCM for uncompressed audio data, and IMA-ADPCM
+ * for compressed audio data). It is not supported in JPEG 2000, PNG, or GIF.
+ */
