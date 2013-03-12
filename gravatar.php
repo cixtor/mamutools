07d3a428539b8ce9c81ccf06c6db9194289a39ec
Cixtor <cixtords@gmail.com>
2013-03-12 09:00:00 -0400

    Added. Gravatar Information Gatherer

---
+++ gravatar.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Gravatar Information Gatherer
+ * http://www.cixtor.com/
+ * https://github.com/cixtor/mamutools
+ *
+ * An avatar is an image that represents a person online, basically a little
+ * picture that appears next to your name when you interact with websites. A
+ * Gravatar is a Globally Recognized Avatar. You upload it and create your
+ * profile just once, and then when you participate in any Gravatar-enabled site,
+ * your Gravatar image will automatically follow you there.
+ *
+ * Gravatar is a free service for site owners, developers, and users. It is
+ * automatically included in every WordPress account and is run and supported
+ * by Automattic.
+ */
