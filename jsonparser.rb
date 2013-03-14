381504189388e38d9540ea78572e8db2428cc5df
Cixtor <cixtords@gmail.com>
2013-03-14 09:00:00 -0400

    Added. JSON Parser & Prettifier

---
+++ jsonparser.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+# JSON Parser & Prettifier
+# http://www.cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/JSON
+#
+# JSON or JavaScript Object Notation, is a text-based open standard designed for
+# human-readable data interchange. It is derived from the JavaScript scripting
+# language for representing simple data structures and associative arrays, called
+# objects. Despite its relationship to JavaScript, it is language-independent,
+# with parsers available for many languages.
+#
+# The JSON format was originally specified by Douglas Crockford, and is described
+# in RFC 4627. The official Internet media type for JSON is application/json. The
+# JSON filename extension is .json. The JSON format is often used for serializing
+# and transmitting structured data over a network connection. It is used primarily
+# to transmit data between a server and web application, serving as an alternative
+# to XML.
+#
