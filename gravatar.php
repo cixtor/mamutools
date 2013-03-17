acbaacdab5db4e6feea8b7f7a3a5734a909f66dc
Cixtor <cixtords@gmail.com>
2013-03-17 09:00:00 -0400

    Added. Gravatar Information Gatherer: Support for username

--- gravatar.php
+++ gravatar.php
@@ -25,6 +25,10 @@ function get_url($url=''){
     curl_close($curl);
     return array( 'headers'=>$headers, 'content'=>$output );
 }
+function is_email($address=''){
+    $address = htmlspecialchars(trim($address));
+    return (bool) preg_match('/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix', $address);
+}
 function print_array($array=array(), $tabs=0){
     if( is_array($array) ){
         foreach($array as $key=>$value){
@@ -41,16 +45,33 @@ function print_array($array=array(), $tabs=0){
         echo "{$array}\n";
     }
 }
+echo "Gravatar Information Gatherer\n";
+echo "  http://www.cixtor.com/\n";
+echo "  https://github.com/cixtor/mamutools\n";
+echo "\n";
 if( isset($argv[1]) ){
     $email = $argv[1];
-    echo "Retrieving information for: {$email}\n";
-    $gravatar = get_url("https://secure.gravatar.com/".md5($email).".php");
-    if( $gravatar['headers']['http_code']>0 ){
-        print_array( unserialize($gravatar['content']) );
+    echo "Identifier: \033[0;93m{$email}\033[0m\n";
+    if( is_email($email) ){
+        echo "Identifier is email? \033[0;92mTrue\033[0m\n";
+        $gravatar_url = 'https://secure.gravatar.com/'.md5($email).'.php';
     }else{
-        echo "[-] Error, the remote content couldn't be reached.\n";
+        echo "Identifier is username? \033[0;92mTrue\033[0m\n";
+        $gravatar_url = 'http://en.gravatar.com/'.urlencode($email).'.php';
+    }
+    $gravatar = get_url($gravatar_url);
+    //
+    if( $gravatar['headers']['http_code']>0 ){
+        $gravatar_data = unserialize($gravatar['content']);
+        if( $gravatar['headers']['http_code']==200 ){
+            print_array($gravatar_data);
+        }else{
+            echo "\033[0;91m[x] Error.\033[0m {$gravatar_data}\n";
+        }
+    }else{
+        echo "\033[0;91m[x] Error.\033[0m The remote content couldn't be reached.\n";
     }
 }else{
-    echo "Usage: ".__FILE__." username@domain.com\n";
+    echo "Usage: ".basename(__FILE__)." username@domain.com\n";
 }
 /* EOF */
