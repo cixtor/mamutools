411479969b126ecccc96cb71a8e8f6c0a89b2b98
Cixtor <cixtords@gmail.com>
2013-04-03 09:00:00 -0400

    Added. Pastebin Tool

---
+++ pastebin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Pastebin Getter
+# http://www.cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/Pastebin
+#
+# A Pastebin is a type of web application where anyone can store text for a
+# certain period of time. This type of website is mainly used by programmers
+# to store pieces of source code or configuration information, but anyone can
+# basically share any type of text. The idea behind pastebins is to make it
+# more convenient for people to share large amounts of text online.
+#
+# A vast number of pastebin related websites exist on the Internet, suiting
+# a number of different needs and providing features tailored towards the
+# crowd they focus on most.
+#
