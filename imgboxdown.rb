d4608cf1410bce04ebae03976e866bb6a2f8dd83
Cixtor <cixtords@gmail.com>
2013-04-07 09:00:00 -0400

    Added. Imgbox Downloader

---
+++ imgboxdown.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#
+# Imgbox Downloader
+# http://www.cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://imgbox.com/
+#
+# * What is imgbox?
+#   Imgbox is a free image hosting service.
+# * What file types are supported?
+#   Images of the file types JPG, GIF and PNG are supported.
+# * What is the maximum file size?
+#   Each file must be 10MB or smaller.
+# * How long are images stored?
+#   Images are stored for life time (see our terms of service).
+# * Should I keep a backup of my images on my local machine?
+#   Yes, you always should backup your files.
+# * I registered for a free account. How many images can I store?
+#   There are no limits (fair use).
+# * Can I inline link (hotlink) the original images I uploaded?
+#   Yes, but please use the provided "full size" share codes only. Other URLs may change in the future.
+# * Are there bandwidth limitations for inlin linking (hotlinking)?
+#   There is no hard limit. Abusive referrers will be blocked.
+# * Why are my thumbnail previews square?
+#   Thumbnails on your "My Images" and "My Galleries" pages are displayed as squares to preserve the layout.
+#
