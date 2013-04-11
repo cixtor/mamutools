03c4d9ba8aa2f2e806dd6ef70d2fc2fc69376282
Cixtor <cixtords@gmail.com>
2013-04-11 09:00:00 -0400

    Added. Imgbox Downloader: OptionParser support

--- imgboxdown.rb
+++ imgboxdown.rb
@@ -24,37 +24,68 @@
 # * Why are my thumbnail previews square?
 #   Thumbnails on your "My Images" and "My Galleries" pages are displayed as squares to preserve the layout.
 #
-album_id = !ARGV[0].nil? ? ARGV[0].to_s : nil
-user_agent = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-#
-if match = album_id.match(/imgbox\.com\/g\/([a-zA-Z0-9]{10})/) then
-    album_id = match[1]
-elsif album_id.length > 10 then
-    album_id = nil
-    puts "Error. Invalid album identifier."
+require 'optparse'
+def success(message)
+    puts "\e[0;92mOK.\e[0m #{message}"
 end
-if !album_id.nil? then
-    gallery_folder = "imgbox-#{album_id}"
-    if Dir.mkdir(gallery_folder) then
-        Dir.chdir(gallery_folder)
-        album_content = %x{curl --silent 'http://imgbox.com/g/#{album_id}'}
-        album_lines = album_content.split("\n")
-        album_lines.each do |line|
-            if match = line.chomp.match(/<a href="\/(.*)" class="gallery_img"><img alt="(.*)" src="(.*)" \/><\/a>/) then
-                image_content = %x{curl --silent 'http://imgbox.com/#{match[1]}'}
-                image_lines = image_content.split("\n")
-                image_lines.each do |image_line|
-                    if image_match = image_line.chomp.match(/<img alt=".*" class="box" id="img" onclick=".*" src="(.*)" title="(.*)" \/>/) then
-                        remote_image = image_match[1].gsub('&amp;', '&')
-                        puts "Downloading '#{remote_image}' as '#{image_match[2]}'"
-                        %x{wget --quiet --user-agent='#{user_agent}' '#{remote_image}' -O '#{image_match[2]}'}
-                    end
+def highlight(message)
+    puts "\e[0;93m[>]\e[0m #{message}"
+end
+def fail(message)
+    puts "\e[0;91m[x]\e[0m #{message}"
+    exit(1)
+end
+def usage
+    puts 'Imgbox Downloader'
+    puts '    http://www.cixtor.com/'
+    puts '    https://github.com/cixtor/mamutools'
+    puts '    http://imgbox.com/'
+    puts
+end
+config = {
+    :filename => __FILE__,
+    :user_agent => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+    :album_id => nil
+}
+#
+usage
+OptionParser.new do |option|
+    option.banner = "Usage: #{config[:filename]} --id 'qqXcABLVX4'"
+    option.on('-i arg', '--id=arg', 'Specify the gallery identifier (ten characters).'){ |arg| config[:album_id] = arg }
+end.parse!
+#
+if match = config[:album_id].match(/imgbox\.com\/g\/([a-zA-Z0-9]{10})/) then
+    config[:album_id] = match[1]
+elsif album_id.length > 10 then
+    fail 'Error. Invalid album identifier.'
+end
+if !config[:album_id].nil? then
+    gallery_folder = "imgbox-#{config[:album_id]}"
+    if File.exists?(gallery_folder) then
+        success "Gallery folder already exists: \e[0;93m#{gallery_folder}\e[0m"
+    else
+        if Dir.mkdir(gallery_folder) then
+            Dir.chdir(gallery_folder)
+            success "Gallery folder created: \e[0;93m#{gallery_folder}\e[0m"
+        else
+            fail "Could not create gallery folder: \e[0;93m#{gallery_folder}\e[0m"
+        end
+    end
+    album_content = %x{curl --silent 'http://imgbox.com/g/#{config[:album_id]}'}
+    album_lines = album_content.split("\n")
+    album_lines.each do |line|
+        if match = line.chomp.match(/<a href="\/(.*)" class="gallery_img"><img alt="(.*)" src="(.*)" \/><\/a>/) then
+            image_content = %x{curl --silent --user-agent '#{config[:user_agent]}' 'http://imgbox.com/#{match[1]}'}
+            image_lines = image_content.split("\n")
+            image_lines.each do |image_line|
+                if image_match = image_line.chomp.match(/<img alt=".*" class="box" id="img" onclick=".*" src="(.*)" title="(.*)" \/>/) then
+                    remote_image = image_match[1].gsub('&amp;', '&')
+                    highlight "Downloading '#{remote_image}' as '#{image_match[2]}'"
+                    %x{wget --quiet --user-agent='#{config[:user_agent]}' '#{remote_image}' -O '#{image_match[2]}'}
                 end
             end
         end
-    else
-        puts "Error. Could not create gallery folder."
-        exit(1)
     end
+    success 'Finished'
 end
 #
