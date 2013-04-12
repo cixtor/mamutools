9967d30082039c6c8d3bc1c2a70d52bb341b1c64
Cixtor <cixtords@gmail.com>
2013-04-12 09:00:00 -0400

    Added. Imgbox Downloader: OptionParser exception handling

--- imgboxdown.rb
+++ imgboxdown.rb
@@ -47,45 +47,51 @@ config = {
     :user_agent => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
     :album_id => nil
 }
-#
-usage
-OptionParser.new do |option|
+optparse = OptionParser.new do |option|
     option.banner = "Usage: #{config[:filename]} --id 'qqXcABLVX4'"
     option.on('-i arg', '--id=arg', 'Specify the gallery identifier (ten characters).'){ |arg| config[:album_id] = arg }
-end.parse!
-#
-if match = config[:album_id].match(/imgbox\.com\/g\/([a-zA-Z0-9]{10})/) then
-    config[:album_id] = match[1]
-elsif album_id.length > 10 then
-    fail 'Error. Invalid album identifier.'
 end
-if !config[:album_id].nil? then
-    gallery_folder = "imgbox-#{config[:album_id]}"
-    if File.exists?(gallery_folder) then
-        success "Gallery folder already exists: \e[0;93m#{gallery_folder}\e[0m"
-    else
-        if Dir.mkdir(gallery_folder) then
-            Dir.chdir(gallery_folder)
-            success "Gallery folder created: \e[0;93m#{gallery_folder}\e[0m"
-        else
-            fail "Could not create gallery folder: \e[0;93m#{gallery_folder}\e[0m"
-        end
+#
+usage
+begin
+    optparse.parse!
+    if match = config[:album_id].match(/imgbox\.com\/g\/([a-zA-Z0-9]{10})/) then
+        config[:album_id] = match[1]
+    elsif album_id.length > 10 then
+        fail 'Error. Invalid album identifier.'
     end
-    album_content = %x{curl --silent 'http://imgbox.com/g/#{config[:album_id]}'}
-    album_lines = album_content.split("\n")
-    album_lines.each do |line|
-        if match = line.chomp.match(/<a href="\/(.*)" class="gallery_img"><img alt="(.*)" src="(.*)" \/><\/a>/) then
-            image_content = %x{curl --silent --user-agent '#{config[:user_agent]}' 'http://imgbox.com/#{match[1]}'}
-            image_lines = image_content.split("\n")
-            image_lines.each do |image_line|
-                if image_match = image_line.chomp.match(/<img alt=".*" class="box" id="img" onclick=".*" src="(.*)" title="(.*)" \/>/) then
-                    remote_image = image_match[1].gsub('&amp;', '&')
-                    highlight "Downloading '#{remote_image}' as '#{image_match[2]}'"
-                    %x{wget --quiet --user-agent='#{config[:user_agent]}' '#{remote_image}' -O '#{image_match[2]}'}
+    if !config[:album_id].nil? then
+        gallery_folder = "imgbox-#{config[:album_id]}"
+        if File.exists?(gallery_folder) then
+            success "Gallery folder already exists: \e[0;93m#{gallery_folder}\e[0m"
+        else
+            if Dir.mkdir(gallery_folder) then
+                Dir.chdir(gallery_folder)
+                success "Gallery folder created: \e[0;93m#{gallery_folder}\e[0m"
+            else
+                fail "Could not create gallery folder: \e[0;93m#{gallery_folder}\e[0m"
+            end
+        end
+        album_content = %x{curl --silent 'http://imgbox.com/g/#{config[:album_id]}'}
+        album_lines = album_content.split("\n")
+        album_lines.each do |line|
+            if match = line.chomp.match(/<a href="\/(.*)" class="gallery_img"><img alt="(.*)" src="(.*)" \/><\/a>/) then
+                image_content = %x{curl --silent --user-agent '#{config[:user_agent]}' 'http://imgbox.com/#{match[1]}'}
+                image_lines = image_content.split("\n")
+                image_lines.each do |image_line|
+                    if image_match = image_line.chomp.match(/<img alt=".*" class="box" id="img" onclick=".*" src="(.*)" title="(.*)" \/>/) then
+                        remote_image = image_match[1].gsub('&amp;', '&')
+                        highlight "Downloading '#{remote_image}' as '#{image_match[2]}'"
+                        %x{wget --quiet --user-agent='#{config[:user_agent]}' '#{remote_image}' -O '#{image_match[2]}'}
+                    end
                 end
             end
         end
+        success 'Finished'
     end
-    success 'Finished'
+rescue OptionParser::MissingArgument, OptionParser::InvalidOption
+    p optparse
+    fail "Process failed, #{$!.to_s}"
 end
+#
 #
