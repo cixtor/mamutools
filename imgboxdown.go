ae20d52cbe059bc0a58ceb6ddf432741ba2a090b
Cixtor <cixtords@gmail.com>
2013-07-05 09:51:06 -0500

    Fixed coding standard in the imgbox downloader program

--- imgboxdown.go
+++ imgboxdown.go
@@ -17,13 +17,13 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "flag"
-    "net/http"
-    "io/ioutil"
-    "strings"
-    "regexp"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
 )
 
 var photo_id = flag.String("photo", "", "Specify the photo identifier to download")
@@ -31,105 +31,105 @@ var gallery_id = flag.String("gallery", "", "Specify the gallery identifier to d
 
 const user_agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 
-func get_remote_content(url string) ([]byte) {
-    client := &http.Client{ }
-    req, err1 := http.NewRequest("GET", url, nil)
+func getRemoteContent(url string) []byte {
+	client := &http.Client{}
+	req, err1 := http.NewRequest("GET", url, nil)
 
-    if err1 == nil {
-        req.Header.Set("User-Agent", user_agent)
-        resp, err2 := client.Do(req)
+	if err1 == nil {
+		req.Header.Set("User-Agent", user_agent)
+		resp, err2 := client.Do(req)
 
-        if err2 == nil {
-            defer resp.Body.Close()
-            body, _ := ioutil.ReadAll(resp.Body)
-            return body
-        }
-    }
+		if err2 == nil {
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+			return body
+		}
+	}
 
-    return nil
+	return nil
 }
 
-func get_remote_content_splitted(url string) ([]string) {
-    var body []byte = get_remote_content(url)
-    var str_lines []string = strings.Split(string(body), "\n")
+func getRemoteContentSplitted(url string) []string {
+	var body []byte = getRemoteContent(url)
+	var str_lines []string = strings.Split(string(body), "\n")
 
-    return str_lines
+	return str_lines
 }
 
-func get_photo(photo_id string) {
-    var r = regexp.MustCompile(`<img alt=".*" class=".*" id="img" onclick=".*" src="(.*)" title="(.*)" \/>`)
-    var photo_url string = fmt.Sprintf("http://imgbox.com/%s", photo_id)
-    var image_lines []string = get_remote_content_splitted(photo_url)
+func getPhoto(photo_id string) {
+	var r = regexp.MustCompile(`<img alt=".*" class=".*" id="img" onclick=".*" src="(.*)" title="(.*)" \/>`)
+	var photo_url string = fmt.Sprintf("http://imgbox.com/%s", photo_id)
+	var image_lines []string = getRemoteContentSplitted(photo_url)
 
-    for _, line := range image_lines {
-        var results []string = r.FindStringSubmatch(line)
+	for _, line := range image_lines {
+		var results []string = r.FindStringSubmatch(line)
 
-        if len(results) > 0 {
-            fmt.Printf( "--> Downloading '%s' as '%s'\n", results[1], results[2] )
-            var filedata []byte = get_remote_content(results[1])
-            ioutil.WriteFile( results[2], filedata, 0644 )
-        }
-    }
+		if len(results) > 0 {
+			fmt.Printf("--> Downloading '%s' as '%s'\n", results[1], results[2])
+			var filedata []byte = getRemoteContent(results[1])
+			ioutil.WriteFile(results[2], filedata, 0644)
+		}
+	}
 }
 
-func get_gallery(gallery_id string) {
-    var gallery_folder string = fmt.Sprintf("imgbox-%s", gallery_id)
-    _, err1 := os.Stat(gallery_folder)
+func getGallery(gallery_id string) {
+	var gallery_folder string = fmt.Sprintf("imgbox-%s", gallery_id)
+	_, err1 := os.Stat(gallery_folder)
 
-    if err1 == nil {
-        fmt.Printf( "OK. Gallery folder already exists: %s\n", gallery_folder )
-    } else {
-        err2 := os.Mkdir(gallery_folder, 0755)
+	if err1 == nil {
+		fmt.Printf("OK. Gallery folder already exists: %s\n", gallery_folder)
+	} else {
+		err2 := os.Mkdir(gallery_folder, 0755)
 
-        if err2 == nil {
-            fmt.Printf( "OK. Gallery folder created: %s\n", gallery_folder )
-        } else {
-            fmt.Printf( "[x] Could not create gallery folder: %s\n", gallery_folder )
-            os.Exit(1)
-        }
-    }
+		if err2 == nil {
+			fmt.Printf("OK. Gallery folder created: %s\n", gallery_folder)
+		} else {
+			fmt.Printf("[x] Could not create gallery folder: %s\n", gallery_folder)
+			os.Exit(1)
+		}
+	}
 
-    os.Chdir(gallery_folder)
+	os.Chdir(gallery_folder)
 
-    var r = regexp.MustCompile(`<a href="\/(.*)"><img alt=".*" src="[a-z]+:\/\/s\.imgbox\.com\/.*" \/><\/a>`)
-    var album_url string = fmt.Sprintf("http://imgbox.com/g/%s", gallery_id)
-    var album_lines []string = get_remote_content_splitted(album_url)
+	var r = regexp.MustCompile(`<a href="\/(.*)"><img alt=".*" src="[a-z]+:\/\/s\.imgbox\.com\/.*" \/><\/a>`)
+	var album_url string = fmt.Sprintf("http://imgbox.com/g/%s", gallery_id)
+	var album_lines []string = getRemoteContentSplitted(album_url)
 
-    for _, line := range album_lines {
-        var results []string = r.FindStringSubmatch(line)
+	for _, line := range album_lines {
+		var results []string = r.FindStringSubmatch(line)
 
-        if len(results) > 0 {
-            get_photo(results[1])
-        }
-    }
+		if len(results) > 0 {
+			getPhoto(results[1])
+		}
+	}
 }
 
 func download(photo_id string, gallery_id string) {
-    if photo_id != "" {
-        fmt.Printf( "OK. Download photo: %s\n", photo_id )
-        get_photo(photo_id)
-    } else if ( gallery_id != "" ){
-        fmt.Printf( "OK. Download gallery: %s\n", gallery_id )
-        get_gallery(gallery_id)
-    } else {
-        fmt.Printf("Missing argument: -photo or -gallery\n")
-        flag.Usage()
-        os.Exit(1)
-    }
+	if photo_id != "" {
+		fmt.Printf("OK. Download photo: %s\n", photo_id)
+		getPhoto(photo_id)
+	} else if gallery_id != "" {
+		fmt.Printf("OK. Download gallery: %s\n", gallery_id)
+		getGallery(gallery_id)
+	} else {
+		fmt.Printf("Missing argument: -photo or -gallery\n")
+		flag.Usage()
+		os.Exit(1)
+	}
 }
 
 func main() {
-    flag.Usage = func(){
-        fmt.Printf("Imgbox Downloader\n")
-        fmt.Printf("  http://cixtor.com/\n")
-        fmt.Printf("  https://github.com/cixtor/mamutools\n")
-        fmt.Printf("  http://imgbox.com/\n")
-        fmt.Printf("Usage:\n")
-        flag.PrintDefaults()
-    }
+	flag.Usage = func() {
+		fmt.Printf("Imgbox Downloader\n")
+		fmt.Printf("  http://cixtor.com/\n")
+		fmt.Printf("  https://github.com/cixtor/mamutools\n")
+		fmt.Printf("  http://imgbox.com/\n")
+		fmt.Printf("Usage:\n")
+		flag.PrintDefaults()
+	}
 
-    flag.Parse()
+	flag.Parse()
 
-    download( *photo_id, *gallery_id )
-    fmt.Printf( "OK. Finished\n" )
+	download(*photo_id, *gallery_id)
+	fmt.Printf("OK. Finished\n")
 }
