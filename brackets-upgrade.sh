60b6614d4663cf237734543a9edb35dfce646c30
Cixtor <cixtords@gmail.com>
2013-10-08 16:14:10 -0400

    Added. Brackets Upgrade

---
+++ brackets-upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Brackets Upgrade
+# http://www.cixtor.com/
+# https://github.com/cixtor/mamutools
+# https://github.com/adobe/brackets
+#
+# Brackets is an open source code editor for web designers and front-end developers.
+#
+# Upgrading is the process of replacing a product with a newer version of the same
+# product. In computing and consumer electronics an upgrade is generally a replacement
+# of hardware, software or firmware with a newer or better version, in order to bring
+# the system up to date or to improve its characteristics.
+#
+# Examples of common hardware upgrades include installing additional memory (RAM),
+# adding larger hard disks, replacing microprocessor cards or graphics cards, and
+# installing new versions of software. Many other upgrades are often possible as
+# well. Common software upgrades include changing the version of an operating system,
+# of an office suite, of an anti-virus program, or of various other tools.
+#
+# Although developers usually produce upgrades in order to improve a product, there
+# are risks involved—including the possibility that the upgrade will worsen the product.
+# Upgrades can also worsen a product subjectively. A user may prefer an older version
+# even if a newer version functions perfectly as designed.
+#
