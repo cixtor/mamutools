d5ca58190a377d2b67172de61ccd596587c378ea
Cixtor <cixtords@gmail.com>
2013-10-28 17:45:06 -0500

    Added. Printline (print specific lines)

---
+++ printline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Catline
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/Cat_(Unix)
+#
+# The cat program is a standard Unix utility that concatenates and lists files. The
+# name is an abbreviation of catenate, a synonym of concatenate. The Single Unix
+# Specification specifies that when the "cat" program is given files in a sequence
+# as arguments, it will output their contents to the standard output in the same
+# sequence. It mandates the support of one option flag, u (unbuffered), by which
+# each byte is written to standard output without buffering as it is read. Many
+# operating systems do this by default and ignore the flag.
+#
