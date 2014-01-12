d28c7c5ea84886270c54982256761384b6988451
Cixtor <cixtords@gmail.com>
2014-01-12 10:41:27 -0500

    Added. Remote Assets

---
+++ remoteassets.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#
+# Remote Assets
+# http://www.cixtor.com/
+# https://github.com/cixtor/mamutools
+#
+# List all the resources (assets) loaded in a specific page on a website.
+#
+# A digital asset is any item of text or media that has been formatted into a
+# binary source that includes the right to use it. A digital file without the
+# right to use it is not an asset. Digital assets are categorised in three major
+# groups which may be defined as textual content (digital assets), images (media
+# assets) and multimedia (media assets).
+#
