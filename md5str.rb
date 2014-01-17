9262d2501ed8c1423974fa8f928b5ee1772b8d48
Cixtor <cixtords@gmail.com>
2014-01-17 10:41:27 -0500

    Added. MD5 String

---
+++ md5str.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#
+# MD5 String
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/MD5
+#
+# Display the text string specified to MD5.
+#
+# The MD5 message-digest algorithm is a widely used cryptographic hash function
+# producing a 128-bit (16-byte) hash value, typically expressed in text format
+# as a 32 digit hexadecimal number. MD5 has been utilized in a wide variety of
+# cryptographic applications, and is also commonly used to verify data integrity.
+#
