e937015520df45def5cee2f1ee2e4c1dc964ff5b
Cixtor <cixtords@gmail.com>
2014-02-15 10:33:20 -0500

    Added. String Conversion: Lowercase whole text string

--- strconv.go
+++ strconv.go
@@ -42,6 +42,7 @@ var new_str = flag.String("new", "", "Text string that will replace the old one"
 var replace = flag.Bool("replace", false, "Replace a text string with another")
 var capitalize = flag.Bool("capitalize", false, "Convert a text string into a capitalized version of its words")
 var uppercase = flag.Bool("uppercase", false, "Convert all the characters in a text string into their capital form")
+var lowercase = flag.Bool("lowercase", false, "Convert all the characters in a text string into their lower form")
 
 func main() {
     flag.Usage = func(){
@@ -68,6 +69,10 @@ func main() {
         *action = "uppercase"
     }
 
+    if *lowercase == true {
+        *action = "lowercase"
+    }
+
     switch *action {
     case "replace":
         fmt.Printf( "%s\n", strings.Replace(*text, *old_str, *new_str, -1) )
@@ -75,5 +80,7 @@ func main() {
         fmt.Printf( "%s\n", strings.Title(*text) )
     case "uppercase":
         fmt.Printf( "%s\n", strings.ToUpper(*text) )
+    case "lowercase":
+        fmt.Printf( "%s\n", strings.ToLower(*text) )
     }
 }
