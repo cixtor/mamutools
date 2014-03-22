505a9896c4069830a6fab012c2193c9735264d39
Cixtor <cixtords@gmail.com>
2014-03-22 09:55:39 -0400

    Added. Environment Debugger: Display all variables

--- environ.go
+++ environ.go
@@ -28,3 +28,43 @@
  * - MAIL - used to indicate where a user's mail is to be found.
  * - TEMP - location where processes can store temporary files
  */
+
+package main
+
+import "os"
+import "fmt"
+import "flag"
+import "regexp"
+
+var display_all = flag.Bool("all", false, "Display all the environment variables as a list")
+
+func main() {
+    flag.Parse()
+
+    var envvars []string = os.Environ();
+
+    if *display_all {
+        r := regexp.MustCompile(`^([a-zA-Z_]+)=(.*)`)
+        var envvar_array = make(map[string]string)
+        var longest_name int = 0
+
+        for _, envvar_str := range envvars {
+            var envvar_parts []string = r.FindStringSubmatch(envvar_str)
+
+            if envvar_parts != nil {
+                var envvar_name string = envvar_parts[1]
+                var envvar_value string = envvar_parts[2]
+                var envvar_length int = len(envvar_parts[1])
+
+                envvar_array[envvar_name] = envvar_value
+                if envvar_length > longest_name {
+                    longest_name = envvar_length
+                }
+            }
+        }
+
+        for envvar_name, envvar_value := range envvar_array {
+            fmt.Printf( "%*s = %s\n", longest_name, envvar_name, envvar_value )
+        }
+    }
+}
