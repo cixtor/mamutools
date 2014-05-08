4a1d6db5d81c8b17ae15398511c39e9fb14ceb1a
Cixtor <cixtords@gmail.com>
2014-05-08 09:40:42 -0400

    Added. File Server: Validate port number

--- fileserver.go
+++ fileserver.go
@@ -21,15 +21,36 @@
 package main
 
 import (
+    "os"
+    "fmt"
     "flag"
     "net/http"
+    "regexp"
 )
 
 var dir_path = flag.String("path", "./", "Set the directory path where the server will run")
 var server_port = flag.String("port", "8080", "Set the port number where the server will run")
 
 func main() {
+    flag.Usage = func(){
+        fmt.Println("File Server")
+        fmt.Println("  http://cixtor.com/")
+        fmt.Println("  https://github.com/cixtor/mamutools")
+        fmt.Println("  http://en.wikipedia.org/wiki/File_server")
+        fmt.Println("Usage:")
+        flag.PrintDefaults()
+    }
+
     flag.Parse()
+
+    port_re := regexp.MustCompile(`^[0-9]{2,4}$`)
+    var port_match []string = port_re.FindStringSubmatch(*server_port)
+
+    if port_match == nil {
+        flag.Usage()
+        fmt.Printf("\nError. Invalid port number.\n")
+        os.Exit(1)
+    }
 
     http.Handle("/", http.FileServer(http.Dir(*dir_path)))
     http.ListenAndServe( ":" + *server_port, nil )
