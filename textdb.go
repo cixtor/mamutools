d9b7c8262e8f519a91235f217c8f5cab1cf53715
Cixtor <cixtords@gmail.com>
2014-05-15 09:49:53 -0400

    Added. Text Database

---
+++ textdb.go
@@ -0,0 +1,23 @@
+/**
+ * Text Database
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/Flat_file_database
+ *
+ * A flat file database describes any of various means to encode a database
+ * model (most commonly a table) as a single file. A flat file can be a plain
+ * text file or a binary file. There are usually no structural relationships
+ * between the records.
+ *
+ * Plain text files usually contain one record per line,[2] There are different
+ * conventions for depicting data. In comma-separated values and delimiter-
+ * separated values files, fields can be separated by delimiters such as comma
+ * or tab characters. In other cases, each field may have a fixed length; short
+ * values may be padded with space characters. Extra formatting may be needed to
+ * avoid delimiter collision. More complex solutions are markup languages and
+ * programming languages.
+ *
+ * Typical examples of flat files are /etc/passwd and /etc/group on Unix-like
+ * operating systems. Another example of a flat file is a name-and-address list
+ * with the fields Name, Address, and Phone Number.
+ */
