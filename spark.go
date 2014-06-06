8e430a0d189c62d4602957f5eedfeb435ec9cc96
Cixtor <cixtords@gmail.com>
2014-06-06 09:46:55 -0400

    Added. Sparkline generator

---
+++ spark.go
@@ -0,0 +1,19 @@
+/**
+ * Spark
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/Sparkline
+ *
+ * A sparkline is a very small line chart, typically drawn without axes or
+ * coordinates. It presents the general shape of the variation (typically over
+ * time) in some measurement, such as temperature or stock market price, in a
+ * simple and highly condensed way. Sparklines are small enough to be embedded
+ * in text, or several sparklines may be grouped together as elements of a small
+ * multiple.
+ *
+ * Whereas the typical chart is designed to show as much data as possible, and
+ * is set off from the flow of text, sparklines are intended to be succinct,
+ * memorable, and located where they are discussed.
+ *
+ * PS. All credits go to @holman by its original implementation in Bash.
+ */
