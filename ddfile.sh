baf829789948de083505e2106b87ced39bf40453
Cixtor <cixtords@gmail.com>
2014-10-09 09:30:07 -0400

    Added. DD File (zero-filled file with bytes)

---
+++ ddfile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# DD File
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/Dd_(Unix)
+#
+# DD (Data Description) is a command on Unix and Unix-like operating systems whose
+# primary purpose is to convert and copy a file. On Unix, device drivers for
+# hardware (such as hard disks) and special device files (such as /dev/zero and
+# /dev/random) appear in the file system just like normal files; the command can
+# also read and/or write from/to these files, provided that function is
+# implemented in their respective driver. As a result, it can be used for tasks
+# such as backing up the boot sector of a hard drive, and obtaining a fixed amount
+# of random data. The program can also perform conversions on the data as it is
+# copied, including byte order swapping and conversion to and from the ASCII and
+# EBCDIC text encodings.
+#
