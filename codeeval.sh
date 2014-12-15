7485fda0d72028a3b11699525d1f1867934e2547
Cixtor <cixtords@gmail.com>
2014-12-15 09:00:18 -0400

    Added. CodeEval Tester

---
+++ codeeval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# CodeEval
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://www.codeeval.com/
+#
+# CodeEval is a platform used by developers to showcase their skills. Developers
+# can participate in app building competitions and win cash/prizes. They can also
+# solve programming challenges as a way to impress employers with their technical
+# skills. Employers can use CodeEval as a way to enhance their brand by launching
+# competitions/programming challenges and as a means to get introduced to the best
+# developers.
+#
