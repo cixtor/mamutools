4794e00bad14b3c420ea0987e29bc46a3092d1e7
Cixtor <cixtords@gmail.com>
2014-12-20 09:00:18 -0400

    Added. CodeEval Tester: Shell extension support

--- codeeval.sh
+++ codeeval.sh
@@ -26,6 +26,7 @@ if [[ "$1" != "" ]]; then
     case $extension in
         'cpp' ) exec_command="g++ -o test.bin ${solution_file} && ./test.bin %s && rm test.bin";;
         'go' ) exec_command="go run ${solution_file} %s";;
+        'sh' ) exec_command="bash ${solution_file} %s";;
     esac
 
     if [[ "${exec_command}" == "null" ]]; then
