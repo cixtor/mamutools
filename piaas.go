bfc0f2b753375f2cf840aa720db1b54b1124bfd7
Cixtor <cixtords@gmail.com>
2014-12-26 09:00:18 -0400

    Added. PI as a Service

---
+++ piaas.go
@@ -0,0 +1,19 @@
+/**
+ * PI as a Service
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/Pi
+ * http://piaas.org/
+ *
+ * The number PI is a mathematical constant, the ratio of a circle's
+ * circumference to its diameter, commonly approximated as 3.14159. Being an
+ * irrational number, PI cannot be expressed exactly as a common fraction,
+ * although fractions such as 22/7 and other rational numbers are commonly used
+ * to approximate PI. Consequently its decimal representation never ends and
+ * never settles into a permanent repeating pattern. The digits appear to be
+ * randomly distributed; however, to date, no proof of this has been discovered.
+ * Also, PI is a transcendental number, a number that is not the root of any
+ * non-zero polynomial having rational coefficients. This transcendence of PI
+ * implies that it is impossible to solve the ancient challenge of squaring the
+ * circle with a compass and straightedge.
+ */
