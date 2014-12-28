f9094d487d9fb6aa8ec531e0e2d3fe4f83e640ae
Cixtor <cixtords@gmail.com>
2014-12-28 09:00:18 -0400

    Added. Random Domain

---
+++ randomdotcom.go
@@ -0,0 +1,22 @@
+/**
+ * Random Domain
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/.com
+ * http://www.randomdotcom.com/
+ *
+ * The domain name com is a top-level domain (TLD) in the Domain Name System of
+ * the Internet. Its name is derived from the word commercial,[1] indicating its
+ * original intended purpose for domains registered by commercial organizations.
+ * However, eventually the distinction was lost when .com, .org and .net were
+ * opened for unrestricted registration.
+ *
+ * The domain was originally administered by the United States Department of
+ * Defense, but is today operated by Verisign, and remains under ultimate
+ * jurisdiction of U.S. law. Verisign Registrations in com are processed via
+ * registrars accredited by ICANN. The registry accepts internationalized domain
+ * names. The domain was one of the original top-level domains (TLDs) in the
+ * Internet when the Domain Name System was implemented in January 1985, the
+ * others being edu, gov, mil, net, org, and arpa. It has grown into the largest
+ * top-level domain.
+ */
