3542dee82e633a894ac91143d0b1c7e85f6a7b87
Cixtor <cixtords@gmail.com>
2014-12-31 09:00:18 -0400

    Added. Telize GeoLocation

---
+++ telize.go
@@ -0,0 +1,24 @@
+/**
+ * Telize Geo Location
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/String_(computer_science)
+ * http://en.wikipedia.org/wiki/String_functions
+ *
+ * Geolocation is the identification of the real-world geographic location of an
+ * object, such as a radar source, mobile phone or Internet-connected computer
+ * terminal. Geolocation may refer to the practice of assessing the location, or
+ * to the actual assessed location. Geolocation is closely related to the use of
+ * positioning systems but may be distinguished from it by a greater emphasis on
+ * determining a meaningful location (e.g. a street address) rather than just a
+ * set of geographic coordinates.
+ *
+ * Internet and computer geolocation can be performed by associating a
+ * geographic location with the Internet Protocol (IP) address, MAC address,
+ * RFID, hardware embedded article/production number, embedded software number
+ * (such as UUID, Exif/IPTC/XMP or modern steganography), invoice, Wi-Fi
+ * positioning system, device fingerprint, canvas fingerprinting or device GPS
+ * coordinates, or other, perhaps self-disclosed information. Geolocation
+ * usually works by automatically looking up an IP address on a WHOIS service
+ * and retrieving the registrant's physical address.
+ */
