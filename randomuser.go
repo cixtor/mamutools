f8c684ea74ba0ee8e770710e25fd5053b4e8e56b
Cixtor <cixtords@gmail.com>
2015-01-05 09:00:18 -0400

    Added. Random User

---
+++ randomuser.go
@@ -0,0 +1,18 @@
+/**
+ * Random User
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/Randomness
+ * https://randomuser.me/
+ *
+ * Microservice to generate random user data.
+ *
+ * Randomness means lack of pattern or predictability in events. A random
+ * sequence of events, symbols or steps has no order and does not follow an
+ * intelligible pattern or combination. Individual random events are by
+ * definition unpredictable, but in many cases the frequency of different
+ * outcomes over a large number of events or trials is predictable. In this
+ * view, randomness is a measure of uncertainty of an outcome, rather than
+ * haphazardness, and applies to concepts of chance, probability, and
+ * information entropy.
+ */
