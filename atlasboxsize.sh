ec9b36f07f9f3c48295493db086d01b9267669c7
Cixtor <cixtords@gmail.com>
2015-01-18 09:00:15 -0400

    Added. Atlas Hashicorp box size

---
+++ atlasboxsize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Atlas Box Size
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# https://atlas.hashicorp.com/boxes/search
+#
+# Vagrant Cloud was a product HashiCorp launched to support the hosting and
+# discovery of Vagrant Boxes, as well as other features like Vagrant Share.
+# Recently, it has been moved into Atlas. Atlas provides functionality beyond
+# Vagrant Cloud, but still what was initially offered. You can learn more about
+# Atlas on the homepage.
+#
