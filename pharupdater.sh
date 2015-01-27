5cff6deadef7138eec5e72a27c5394e82364db76
Cixtor <cixtords@gmail.com>
2015-01-27 09:00:15 -0400

    Added. Phar Updater

---
+++ pharupdater.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Phar Updater
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://php.net/manual/en/intro.phar.php
+#
+# The phar extension provides a way to put entire PHP applications into a single
+# file called a "phar" (PHP Archive) for easy distribution and installation.
+# Phar archives are best characterized as a convenient way to group several
+# files into a single file. As such, a phar archive provides a way to distribute
+# a complete PHP application in a single file and run it from that file without
+# the need to extract it to disk. Additionally, phar archives can be executed by
+# PHP as easily as any other file, both on the commandline and from a web
+# server. Phar is kind of like a thumb drive for PHP applications.
+#
