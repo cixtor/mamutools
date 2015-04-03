50cd2a2342460a26224bab2bec391959857573f0
Cixtor <cixtords@gmail.com>
2015-04-03 09:00:21 -0400

    Added. Speed Test script

---
+++ speedtest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Speed Test
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# http://en.wikipedia.org/wiki/Time_To_First_Byte
+#
+# Time To First Byte or TTFB is a measurement that is often used as an
+# indication of the responsiveness of a webserver or other network resources. It
+# is the duration from the virtual user making an HTTP request to the first byte
+# of the page being received by the browser. This time is made up of the socket
+# connection time, the time taken to send the HTTP request, and the time taken
+# to get the first byte of the page.
+#
