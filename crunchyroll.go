d196802d961ea71f70e6c6595b1501e6ec42ad13
Cixtor <cixtords@gmail.com>
2015-04-16 09:00:21 -0400

    Added. Crunchyroll script

---
+++ crunchyroll.go
@@ -0,0 +1,13 @@
+/**
+ * Crunchyroll
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * http://en.wikipedia.org/wiki/Crunchyroll
+ *
+ * Crunchyroll is an American website and international online community focused
+ * on video streaming East Asian media including anime, manga, drama, music,
+ * electronic entertainment, and auto racing content. Crunchyroll's distribution
+ * channel and partnership program delivers content to over five million online
+ * community members worldwide. Crunchyroll is funded by The Chernin Group and
+ * TV Tokyo.
+ */
