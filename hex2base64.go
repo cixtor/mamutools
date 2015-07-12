51a1a2f0cc8768c641575306f0d0c6c45d48db62
Cixtor <cixtords@gmail.com>
2015-07-12 09:00:21 -0400

    Added. Hexadecimal to Base64

---
+++ hex2base64.go
@@ -0,0 +1,26 @@
+/**
+ * Hexadecimal to Base64
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * https://en.wikipedia.org/wiki/Hexadecimal
+ * https://en.wikipedia.org/wiki/Base64
+ * http://cryptopals.com/sets/1/challenges/1/
+ *
+ * Hexadecimal (also base 16, or hex) is a positional numeral system with a
+ * radix, or base, of 16. It uses sixteen distinct symbols, most often the
+ * symbols 0–9 to represent values zero to nine, and A, B, C, D, E, F (or
+ * alternatively a, b, c, d, e, f) to represent values ten to fifteen.
+ * Hexadecimal numerals are widely used by computer systems designers and
+ * programmers. Several different notations are used to represent hexadecimal
+ * constants in computing languages; the prefix "0x" is widespread due to its
+ * use in Unix and C (and related operating systems and languages).
+ *
+ * Base64 is a group of similar binary-to-text encoding schemes that represent
+ * binary data in an ASCII string format by translating it into a radix-64
+ * representation. The particular set of 64 characters chosen to represent the
+ * 64 place-values for the base varies between implementations. The general
+ * strategy is to choose 64 characters that are both members of a subset common
+ * to most encodings, and also printable. This combination leaves the data
+ * unlikely to be modified in transit through information systems, such as
+ * email, that were traditionally not 8-bit clean.
+ */
