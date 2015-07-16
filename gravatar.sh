a9bd4b4a874f83ce7432152df05f348ffda63851
Cixtor <cixtords@gmail.com>
2015-07-16 09:00:21 -0400

    Modified. Gravatar Information Gatherer: Rewrite in Bash

---
+++ gravatar.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Gravatar Information Gatherer
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+#
+# An avatar is an image that represents a person online, basically a little
+# picture that appears next to your name when you interact with websites. A
+# Gravatar is a Globally Recognized Avatar. You upload it and create your
+# profile just once, and then when you participate in any Gravatar-enabled site,
+# your Gravatar image will automatically follow you there.
+#
+# Gravatar is a free service for site owners, developers, and users. It is
+# automatically included in every WordPress account and is run and supported
+# by Automattic.
+#
+
+query="$1"
+isemail=$(echo "$query" | grep --quiet '@')
+
+if [[ "$?" -eq 0 ]]; then
+    uniqueid=$(strconv -md5 -text "$query")
+else
+    uniqueid=$(strconv -urlenc -text "$query")
+fi
+
+response=$(
+    curl --silent "https://secure.gravatar.com/${uniqueid}.json" \
+    --header 'dnt: 1' \
+    --header 'accept-language: en-US,en;q=0.8' \
+    --header 'accept-encoding: gzip, deflate, sdch' \
+    --header 'user-agent: Mozilla/5.0 (KHTML, like Gecko) Safari/537.36' \
+    --header 'accept: text/html,application/xhtml+xml,application/xml' \
+    --header 'cache-control: max-age=0' \
+    --compressed
+)
+
+if [[ $(which jq) ]]; then
+    echo "$response" | jq '.'
+elif [[ $(which python) ]]; then
+    echo "$response" | python -m json.tool
+else
+    echo "$response"
+fi
