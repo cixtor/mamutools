c0450fe3473b68596be5e92b271be82a781040c5
Cixtor <cixtords@gmail.com>
2015-07-19 09:00:26 -0400

    Added. VCS (Version Control System) Chart

---
+++ vcschart.go
@@ -0,0 +1,21 @@
+/**
+ * VCS (Version Control System) Chart
+ * http://cixtor.com/
+ * https://github.com/cixtor/mamutools
+ * https://en.wikipedia.org/wiki/Revision_control
+ *
+ * A component of software configuration management, revision control, also
+ * known as version control or source control, is the management of changes to
+ * documents, computer programs, large web sites, and other collections of
+ * information. Changes are usually identified by a number or letter code, each
+ * revision is associated with a timestamp and the person making the change.
+ *
+ * The profile contributions graph is a record of contributions you have made to
+ * VCS (Version Control System) repositories but they are only counted if they
+ * meet certain criteria. Commits will appear on your contributions graph if
+ * they meet all of the following conditions:
+ *
+ * - The commits were made within the past year.
+ * - The commits were made in a standalone repository, not a fork.
+ * - The commits were made in the repository's default branch.
+ */
