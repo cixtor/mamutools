4e555bc84544e6d9ce06f3dad19d8338d88c34c7
Cixtor <cixtords@gmail.com>
2015-09-14 09:00:19 -0400

    Added. Tumblr gallery downloader

---
+++ tumblrdl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Tumblr Downloader
+# https://github.com/cixtor/mamutools
+# https://en.wikipedia.org/wiki/Tumblr
+# https://en.wikipedia.org/wiki/Web_scraping
+#
+# Tumblr (stylized in its logo as tumblr.) is a microblogging platform and
+# social networking website founded by David Karp and owned by Yahoo! Inc. The
+# service allows users to post multimedia and other content to a short-form
+# blog. Users can follow other users' blogs, as well as make their blogs
+# private. Much of the website's features are accessed from the "dashboard"
+# interface, where the option to post content and posts of followed blogs
+# appear.
+#
