d21dae9592c19dd4aca568786e4541dd702398cb
Cixtor <cixtords@gmail.com>
2015-09-23 09:00:36 -0400

    Added. Bitnami LAMP setup

---
+++ bitnamilamp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Bitnami LAMP
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# https://en.wikipedia.org/wiki/LAMP_(software_bundle)
+# https://bitnami.com/stack/lamp
+#
+# LAMP is an archetypal model of web service solution stacks, named as an
+# acronym of the names of its original four open-source components: the Linux
+# operating system, the Apache HTTP Server, the MySQL relational database
+# management system (RDBMS), and the PHP programming language. The LAMP
+# components are largely interchangeable and not limited to the original
+# selection. As a solution stack, LAMP is suitable for building dynamic web
+# sites and web applications.
+#
+# Bitnami LAMP Stack provides a complete, fully-integrated and ready to run LAMP
+# development environment. In addition to PHP, MySQL and Apache, it includes
+# FastCGI, OpenSSL, phpMyAdmin, ModSecurity, SQLite, Varnish, ImageMagick,
+# xDebug, Xcache, OpenLDAP, ModSecurity, Memcache, OAuth, PEAR, PECL, APC, GD,
+# cURL and other components
+#
