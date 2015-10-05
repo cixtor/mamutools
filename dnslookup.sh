db2f997780fe710972217fa7dac2497e4da42aa0
Cixtor <cixtords@gmail.com>
2015-10-05 09:00:36 -0400

    Added. DNS Lookup tool

---
+++ dnslookup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# DNS Lookup
+# http://cixtor.com/
+# https://github.com/cixtor/mamutools
+# https://en.wikipedia.org/wiki/Domain_Name_System
+# https://en.wikipedia.org/wiki/Reverse_DNS_lookup
+# https://en.wikipedia.org/wiki/List_of_DNS_record_types
+#
+# The Domain Name System (DNS) is a hierarchical distributed naming system for
+# computers, services, or any resource connected to the Internet or a private
+# network. It associates various information with domain names assigned to each
+# of the participating entities. Most prominently, it translates domain names,
+# which can be easily memorized by humans, to the numerical IP addresses needed
+# for the purpose of computer services and devices worldwide. The Domain Name
+# System is an essential component of the functionality of most Internet
+# services because it is the Internet's primary directory service.
+#
+# In computer networking, reverse DNS lookup or reverse DNS resolution (rDNS) is
+# the determination of a domain name that is associated with a given IP address
+# using the Domain Name System (DNS) of the Internet.
+#
