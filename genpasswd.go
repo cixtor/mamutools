0549f61e4ef505c84a65ee5478273e70a58f80c4
Cixtor <cixtords@gmail.com>
2018-07-29 09:00:00 -0800

    Modify the business logic to generate random passwords

--- genpasswd.go
+++ genpasswd.go
@@ -5,24 +5,25 @@
  * http://www.youtube.com/watch?v=BIKV3fYmzRQ
  *
  * A random password generator is software program or hardware device that takes
- * input from a random or pseudo-random number generator and automatically generates
- * a password. Random passwords can be generated manually, using simple sources
- * of randomness such as dice or coins, or they can be generated using a computer.
+ * input from a random or pseudo-random number generator and automatically
+ * generates a password. Random passwords can be generated manually, using
+ * simple sources of randomness such as dice or coins, or they can be generated
+ * using a computer.
  *
- * While there are many examples of "random" password generator programs available
- * on the Internet, generating randomness can be tricky and many programs do not
- * generate random characters in a way that ensures strong security. A common
- * recommendation is to use open source security tools where possible, since they
- * allow independent checks on the quality of the methods used. Note that simply
- * generating a password at random does not ensure the password is a strong password,
- * because it is possible, although highly unlikely, to generate an easily guessed
- * or cracked password. In fact there is no need at all for a password to have been
- * produced by a perfectly random process: it just needs to be sufficiently difficult
- * to guess.
+ * While there are many examples of "random" password generator programs
+ * available on the Internet, generating randomness can be tricky and many
+ * programs do not generate random characters in a way that ensures strong
+ * security. A common recommendation is to use open source security tools where
+ * possible, since they allow independent checks on the quality of the methods
+ * used. Note that simply generating a password at random does not ensure the
+ * password is a strong password, because it is possible, although highly
+ * unlikely, to generate an easily guessed or cracked password. In fact there
+ * is no need at all for a password to have been produced by a perfectly random
+ * process: it just needs to be sufficiently difficult to guess.
  *
- * A password generator can be part of a password manager. When a password policy
- * enforces complex rules, it can be easier to use a password generator based on
- * that set of rules than to manually create passwords.
+ * A password generator can be part of a password manager. When a password
+ * policy enforces complex rules, it can be easier to use a password generator
+ * based on that set of rules than to manually create passwords.
  */
 
 package main
@@ -41,23 +42,21 @@ var (
 	length      int
 	howmany     int
 	typeDict    string
-	allTypes    bool
 	customTypes string
-	dictionary  map[string]string
+	dictionary  []string
 )
 
 func init() {
-	flag.IntVar(&length, "length", 10, "Length of each password (default: 10)")
-	flag.IntVar(&howmany, "count", 1, "Quantity of passwords to generate (default: 1)")
-	flag.StringVar(&typeDict, "type", "", "Group of characters to use (one of more): a, A, 1, @")
-	flag.BoolVar(&allTypes, "all", false, "Use all character groups, same as: -type '1a@A'")
-	flag.StringVar(&customTypes, "custom", "", "Custom list of characters for the dictionary")
+	flag.IntVar(&length, "l", 24, "Maximum length for each password")
+	flag.IntVar(&howmany, "c", 1, "How many passwords to generate")
+	flag.StringVar(&typeDict, "t", "", "Group of characters to use (one or more): a, A, 1, @")
+	flag.StringVar(&customTypes, "s", "", "String containing a specific list of usable letters")
 
-	dictionary = map[string]string{
-		"alphabet_minus": "abcdefghijklmnopqrstuvwxyz",
-		"alphabet_mayus": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		"numbers":        "0123456789",
-		"specials":       "!@$%&*_+=-_?/.,:;#",
+	dictionary = []string{
+		"0123456789",                  /* numbers */
+		"~!@#$%^&*()_+-=[]{};:<>,./?", /* specials */
+		"abcdefghijklmnopqrstuvwxyz",  /* alphabet_minus */
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",  /* alphabet_mayus */
 	}
 
 	flag.Usage = func() {
@@ -68,6 +67,15 @@ func init() {
 		fmt.Println("  https://www.youtube.com/watch?v=BIKV3fYmzRQ")
 		fmt.Println("Usage:")
 		flag.PrintDefaults()
+		fmt.Println("Example:")
+		fmt.Println("  genpasswd -t 1      -> only numbers")
+		fmt.Println("  genpasswd -t a      -> only lowercase letters")
+		fmt.Println("  genpasswd -t A      -> only uppercase letters")
+		fmt.Println("  genpasswd -t @      -> only special letters")
+		fmt.Println("  genpasswd -t 1a     -> numbers with lowercase letters")
+		fmt.Println("  genpasswd -t A@     -> uppercase and special letters")
+		fmt.Println("  genpasswd -t 1aA@   -> all kind of letters")
+		fmt.Println("  genpasswd -s \"(:)\"  -> only colons and parens")
 	}
 
 	flag.Parse()
@@ -83,7 +91,7 @@ func main() {
 	userDict := genUserDictionary()
 
 	if userDict == "" {
-		fmt.Println("Dictionary is empty, use -type to populate it")
+		fmt.Println("Dictionary is empty, use -t to populate it")
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -98,41 +106,30 @@ func main() {
 }
 
 func genUserDictionary() string {
+	if customTypes != "" {
+		return customTypes
+	}
+
+	if typeDict == "" {
+		typeDict = "1a@A"
+	}
+
 	var userDict string
 
-	if allTypes {
+	for _, c := range typeDict {
 		for _, values := range dictionary {
-			userDict += values
-		}
-	}
-
-	if typeDict != "" && userDict == "" {
-		if len(typeDict) == 1 {
-			for _, values := range dictionary {
-				if strings.Contains(values, typeDict) {
-					userDict = values
-					break
-				}
-			}
-		} else {
-			for _, c := range typeDict {
-				for _, values := range dictionary {
-					if strings.Contains(values, string(c)) {
-						userDict += values
-					}
-				}
+			if strings.Contains(values, string(c)) {
+				userDict += values
 			}
 		}
-	}
-
-	if customTypes != "" {
-		userDict += customTypes
 	}
 
 	return userDict
 }
 
 func genRandomString(wg *sync.WaitGroup, userDict string, length int) {
+	defer wg.Done()
+
 	var password []byte
 
 	dictlen := int64(len(userDict))
@@ -148,6 +145,4 @@ func genRandomString(wg *sync.WaitGroup, userDict string, length int) {
 	}
 
 	fmt.Printf("%s\n", password)
-
-	defer wg.Done()
 }
